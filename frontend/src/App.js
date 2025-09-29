5633477c635645a0ee68f7af650f5cd86463905d
sirvinaresh <sirvinaresh459@gmail.com>
2025-09-29 11:56:08 +0530

    create header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './Components/Header';
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
       <h1>hello</h1>
+      <Header />
     </div>
   );
 }
