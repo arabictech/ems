e96f8abde438dba3b9e84164ed2d9f8183674fdc
PatelKamya <kamyapatel303@gmail.com>
2025-10-03 11:41:16 +0530

    updates

--- backend/src/main/java/com/employee/ems/controller/UserController.java
+++ backend/src/main/java/com/employee/ems/controller/UserController.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //package com.employee.ems.controller;
 //
 //import com.employee.ems.entity.Users;
@@ -17,6 +18,37 @@
 //    @PostMapping
 //    public Users addUser(@RequestBody Users users) {
 //        return userService.addUser(users);
+=======
+package com.employee.ems.controller;
+
+import com.employee.ems.entity.Users;
+import com.employee.ems.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    UserService  userService;
+
+    @PostMapping
+    public Users addUser(@RequestBody Users users) {
+        return userService.addUser(users);
+    }
+
+    @GetMapping
+    public List<Users> GetallUser(){
+        return userService.GetallUser();
+    }
+
+//    @GetMapping({"/{id}"})
+//    public Users getUserById(@PathVariable("id") long id){
+//        return userService.getUserById(id);
+>>>>>>> ad5b61faf8189393e4e3adb29d8dc256eab1875f
 //    }
 //
 //    @GetMapping
--- backend/src/main/java/com/employee/ems/entity/PayRoll.java
+++ backend/src/main/java/com/employee/ems/entity/PayRoll.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //package com.employee.ems.entity;
 //
 //import jakarta.persistence.*;
@@ -34,3 +35,39 @@
 //
 //
 //}
+=======
+package com.employee.ems.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Month;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "payroll")
+public class PayRoll {
+
+    // Payroll(payroll_id, emp_id, month, salary, deductions, net_salary)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long payroll_id;
+
+    @ManyToOne
+    @JoinColumn(name = "emp_id", nullable = false)
+    private Employees employees;
+
+    private Month month;
+
+    private Double salary;
+    private double deduction;
+    private double net_salary;
+
+}
+>>>>>>> ad5b61faf8189393e4e3adb29d8dc256eab1875f
