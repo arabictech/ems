24ac7614c19ea08528a3831805907dbd2813bc83
PatelKamya <kamyapatel303@gmail.com>
2025-10-13 18:29:42 +0530

    updates in dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,11 @@ import './App.css';
 import Header from './Components/Header';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
-import Employee from './Components/Employees';
 import LeaveForm from './Components/LeaveForm';
-import Login from './Components/Login';
-import RegisterForm from './Components/RegisterForm';
 import Attendance from './Components/Attendance';
-// import Payroll from './Components/Payroll';
 import { SalarySlip } from './Components/SalarySlip';
-import DatePicker from './Components/DatePicker';
 import EmployeeCard from './Components/EmployeeCard';
 import Profile from './Components/Profile';
-import Payroll from './Components/Payroll';
 import TextControlsExample from './Components/AttendenceData';
 import AdminDashboard from './Components/AdminDashboard';
 
@@ -23,15 +17,9 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<AdminDashboard/>}/>
-        {/* <Route path='employee' element={<Employee />}/> */}
         <Route path='/' element={<Dashboard/>}/>
         <Route path='leave' element={<LeaveForm />}/>
-        <Route path='login' element={<Login />}/>
-        {/* <Route path='payroll' element={<RegisterForm />}/>  */}
         <Route path='attendance' element={<Attendance />}/>
-        {/* <Route path='date-picker' element={<DatePicker />}/> */}
-        <Route path='attendance' element={<Login />}/>
-        {/* <Route path='payroll' element={<Payroll />}/> */}
         <Route path='analytics' element={<SalarySlip />}/>
         <Route path='report' element={<Profile />}/>
         <Route path='payroll' element={<TextControlsExample />}/>
